d28ef7e446b12f9f624961df28d328433036ca01
leonnelleijing <leonleijing@gmail.com>
2020-10-19 21:28:20 +0200

    test

--- src/GestionDonnes/Entreprise.java
+++ src/GestionDonnes/Entreprise.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,4 +16,15 @@ public class Entreprise extends Client{
     protected String nomCorrespondant;
     protected String  prenomCorrespondant;
     protected String domaine;
+
+    public Entreprise(String raisonSocial, String noSiret, String nomCorrespondant, String prenomCorrespondant, String domaine, int numClient, String telClient, String rurClient, String villeClient, String codePostalC, String carteBancaire, String noteC) {
+        super(numClient, telClient, rurClient, villeClient, codePostalC, carteBancaire, noteC);
+        this.raisonSocial = raisonSocial;
+        this.noSiret = noSiret;
+        this.nomCorrespondant = nomCorrespondant;
+        this.prenomCorrespondant = prenomCorrespondant;
+        this.domaine = domaine;
+    }
+    
 }
+
--- src/GestionDonnes/Particulier.java
+++ src/GestionDonnes/Particulier.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,4 +14,13 @@ public class Particulier extends Client {
     protected String nomPart;
     protected String  prenomPart;
     protected String civilité;
+
+    public Particulier(String nomPart, String prenomPart, String civilité, int numClient, String telClient, String rurClient, String villeClient, String codePostalC, String carteBancaire, String noteC) {
+        super(numClient, telClient, rurClient, villeClient, codePostalC, carteBancaire, noteC);
+        this.nomPart = nomPart;
+        this.prenomPart = prenomPart;
+        this.civilité = civilité;
+    }
+    
 }
+

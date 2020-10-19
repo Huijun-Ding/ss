0a364f26aa519bd7981b0f95f1e41efc9ebf5757
Huijun-Ding <mathilda.cnfr@gmail.com>
2020-10-19 14:37:00 +0200

    tous les packages

---
+++ src/GestionDonnes/Particulier.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GestionDonnes;
+
+/**
+ *
+ * @author Mathi
+ */
+public class Particulier {
+    
+}

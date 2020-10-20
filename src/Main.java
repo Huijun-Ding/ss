362aa0ab73a5dfa2ca9c9a78133084d448bb64f8
leonnelleijing <leonleijing@gmail.com>
2020-10-20 12:57:51 +0200

    jing

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
 
-<<<<<<< HEAD
-=======
+
 import BD.DB;
 
 
->>>>>>> 89d7dc9e9c2e6696a309e03012478bc7cef1ce70
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-<<<<<<< HEAD
-    }
-    
-}
-=======
-        
-        DB db = new DB("bd_dolt", "jdbc:mysql://localhost:3306/", "root", "");
+        DB db = new DB("dolt", "jdbc:mysql://localhost:3306/", "root", "");
         db.connexion();
+        // TODO code application logic here
+
     }
     
+        
 }
->>>>>>> 89d7dc9e9c2e6696a309e03012478bc7cef1ce70
+    
+
+

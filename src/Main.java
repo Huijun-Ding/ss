4043eab96ed20c5874cc0aa6b1e4f1f910ad7e65
airol123 <carolinerrh0322@gmail.com>
2020-10-20 23:19:08 +0200

    change bd name

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DB db = new DB("dolt", "jdbc:mysql://localhost:3306/", "root", "");
+        DB db = new DB("bd_dolt", "jdbc:mysql://localhost:3306/", "root", "");
         db.connexion();
         // TODO code application logic here
 

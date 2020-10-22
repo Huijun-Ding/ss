7bd8f05faa50e3a671071a6507da0fc34397eef5
airol123 <carolinerrh0322@gmail.com>
2020-10-22 11:40:03 +0200

    update label

--- src/Vue/ConsulterTacheC.java
+++ src/Vue/ConsulterTacheC.java
@@ -12,23 +12,24 @@ public class ConsulterTacheC extends JFrame implements ListSelectionListener {
     private JPanel p;
     private JList liste = new JList();
     private JLabel etat = new JLabel("Etat de tache");
-    private JLabel etatTac;
+    private JLabel etatTac = new JLabel("...");
     private JLabel nom = new JLabel("Nom de tache");
-    private JLabel nomTa ;
+    private JLabel nomTa = new JLabel("...");
     private JLabel nbp = new JLabel("Nombre de personne");
-    private JLabel nbpTa ;
+    private JLabel nbpTa = new JLabel("...");
     private JLabel dateD = new JLabel("Date de debut");
-    private JLabel dateDT ;
+    private JLabel dateDT = new JLabel("...");
     private JLabel dateF = new JLabel("Date d'echeance");
-    private JLabel dateFT ;
+    private JLabel dateFT = new JLabel("...");
     private JLabel Description = new JLabel("Description");
-    private JTextArea Descri=new JTextArea(10, 30);
+    private JTextArea Descri = new JTextArea(10, 30);
     private Client client;
-    private Tache t=new Tache("t1","ed",1,1f,"j",EnumEtat.EN_COURS,"1","2");
+    private Tache t = new Tache("t1", "ed", 1, 1f, "j", EnumEtat.EN_COURS, "1", "2");
     private Tache choix[] = {};
     private String nomChoix[] = {"java"};
 
     JLabel etiquette = new JLabel("                   ");
+
     public ConsulterTacheC() {//Client c
         super("Consulter mes taches");
         //this.client=c;
@@ -37,22 +38,22 @@ public class ConsulterTacheC extends JFrame implements ListSelectionListener {
 
         liste = new JList(nomChoix);
         liste.addListSelectionListener(this);
-        liste.setBounds(0,0,200,600);
+        liste.setBounds(0, 0, 200, 600);
 
 
-        nom.setBounds(250,20,120,20);
-        nomTa.setBounds(390,20,120,20);
-        etat.setBounds(650,20,120,20);
-        etatTac.setBounds(770,20,120,20);
-        dateD.setBounds(250,100,120,20);
-        dateDT.setBounds(390,100,120,20);
-        dateF.setBounds(650,100,120,20);
-        dateFT.setBounds(770,100,120,20);
-        nbp.setBounds(250,180,120,20);
-        nbpTa.setBounds(390,180,120,20);
-        Description.setBounds(250,220,50,100);
-        Descri.setBounds(250,240,600,100);
-        etiquette.setBounds(250,400,50,50);
+        nom.setBounds(250, 20, 120, 20);
+        nomTa.setBounds(390, 20, 120, 20);
+        etat.setBounds(650, 20, 120, 20);
+        etatTac.setBounds(770, 20, 120, 20);
+        dateD.setBounds(250, 100, 120, 20);
+        dateDT.setBounds(390, 100, 120, 20);
+        dateF.setBounds(650, 100, 120, 20);
+        dateFT.setBounds(770, 100, 120, 20);
+        nbp.setBounds(250, 180, 120, 20);
+        nbpTa.setBounds(390, 180, 120, 20);
+        Description.setBounds(250, 220, 50, 100);
+        Descri.setBounds(250, 240, 600, 100);
+        etiquette.setBounds(250, 400, 50, 50);
 
         p.add(liste);
         p.add(nomTa);
@@ -72,8 +73,6 @@ public class ConsulterTacheC extends JFrame implements ListSelectionListener {
         this.add(p);
 
 
-
-
         pack();
 
 
@@ -89,36 +88,34 @@ public class ConsulterTacheC extends JFrame implements ListSelectionListener {
         //与choix中的名字相比，相同的话则在其他位置显示该任务信息
         //给其他的加 人或者任务
         for (int i = 0; i < nomChoix.length; i++) {
-            if (nomChoix[i]==choix[i].getNomTache()) {
+            if (nomChoix[i] == choix[i].getNomTache()) {
                 JLabel etatTac = new JLabel(choix[i].getEtat().toString());
                 JLabel nomTa = new JLabel(choix[i].getNomTache());
-                int nb=choix[i].getNbPersonne();
-                String s=""+nb;// transformer int en String
+                int nb = choix[i].getNbPersonne();
+                String s = "" + nb;// transformer int en String
                 JLabel nbpTa = new JLabel(s);
                 JLabel dateDT = new JLabel(choix[i].getDateDeb());
                 JLabel dateFT = new JLabel(choix[i].getDateFin());
                 Descri.setText(choix[i].getDescription());
-            }
-            else{
-                 JLabel etatTac = new JLabel("...");
-                 JLabel nomTa = new JLabel("...");
-                 JLabel nbpTa = new JLabel("...");
-                 JLabel dateDT = new JLabel("...");
-                 JLabel dateFT = new JLabel("...");
-                 JTextArea Descri=new JTextArea(10, 30);
+            } else {
+                etatTac.setText("...");
+                nomTa.setText("...");
+                nbpTa.setText("...");
+                dateDT.setText("...");
+                dateFT.setText("...");
+                Descri.setText("...");
             }
         }
 
 
     }
-    public void addChoix(){
-        for (int i = 0; i <client.getTaches().size() ; i++) {
-            this.choix[i]=client.getTaches().get(i);
-            this.nomChoix[i]=client.getTaches().get(i).getNomTache();
+
+    public void addChoix() {
+        for (int i = 0; i < client.getTaches().size(); i++) {
+            this.choix[i] = client.getTaches().get(i);
+            this.nomChoix[i] = client.getTaches().get(i).getNomTache();
         }
     }
-
-
 
 
 }
